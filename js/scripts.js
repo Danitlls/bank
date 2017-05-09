acd04c64344fc6c1759783b1383b94d189f8115a
Connor Larsen and Daniela Trulls <daniela@>
2017-05-09 16:15:02 -0700

    create bank account with initial deposit and balance adjust works when inputs are present in both fields, but not one field only

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+// Business Logic
+
+function BankAccount(userName, initialDeposit) {
+  this.userName = userName;
+  this.initialDeposit = initialDeposit;
+}
+var balance = 0;
+
+BankAccount.prototype.initialBalance = function () {
+
+  if (this.initialDeposit != 0) {
+    balance += this.initialDeposit;
+  }
+  return balance;
+}
+
+function Adjust(deposit, withdraw) {
+  this.deposit = deposit;
+  this.withdraw = withdraw;
+}
+
+Adjust.prototype.newBalance = function () {
+  if (this.deposit != 0 && this.withdraw != 0) {
+    balance += this.deposit;
+    balance -= this.withdraw;
+
+  // }else if(this.withdraw == NaN && this.deposit != NaN) {
+  //   balance += this.deposit;
+  // }else if(this.withdraw != NaN && this.deposit == NaN) {
+  //   balance -= this.withdraw;
+  }return balance;
+}
+
+
+// UI Logic
+
+$(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+
+    var userName = $("input#nameInput").val();
+    var initialDeposit = parseInt($("input#initialDeposit").val());
+    console.log(initialDeposit);
+
+    var newDeposit = new BankAccount(userName, initialDeposit);
+
+    $("span#currentBalance").text("$" + newDeposit.initialBalance());
+  });
+
+$("button#transfer").click(function(){
+    var deposit = parseInt($("input#deposit").val());
+    var withdraw = parseInt($("input#withdraw").val());
+    console.log(deposit);
+    console.log(withdraw);
+
+    var newAdjust = new Adjust(deposit, withdraw);
+
+
+
+    $("span#currentBalance").text("$" + newAdjust.newBalance());
+  });
+});
